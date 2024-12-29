aa5f8ef5301965fc01826bbc991812bc2acfb02a
ecomptonn <162512945+ecomptonn@users.noreply.github.com>
2024-12-28 19:34:14 -0700

    Added comments for organization

--- models/User.js
+++ models/User.js
@@ -3,21 +3,21 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     steamId: String,
     displayName: String,
-    avatar: String,
-    profileUrl: String,
+    avatar: String, // URL to steam profile pic
+    profileUrl: String, // Steam custom profile url
 
     friends: [{
         steamId: String,
         displayName: String,
-        avatar: String,
-        status: String,
-        currentGame: String,
+        avatar: String, // URL to friends profile pic
+        status: String, // online, offline, etc
+        currentGame: String, // current game friend is playing
     }],
 
     games: [{
-        appId: String,
-        name: String,
-        playtime: Number,
-        lastPlayed: Date,
+        appId: String, // Steam's game ID
+        name: String, // Name of steam game
+        playtime: Number, // total minutes played
+        lastPlayed: Date, // last time played
     }],
 })
