1cf1e7a1328e5b879c928137db8cf680aea7ad80
ecomptonn <162512945+ecomptonn@users.noreply.github.com>
2025-01-06 16:45:57 -0700

    Created privacy route, updated views file tree and finished privacy html layout

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ router.get("/", (req, res) => {
     res.render("pages/dashboard/home");
 });
 
+// @desc    Privacy Policy
+// @route   GET /privacy
+router.get("/privacy", (req, res) => {
+    res.render("pages/privacy");
+});
+
 export default router;
