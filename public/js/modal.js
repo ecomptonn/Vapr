145fbd596289413c3c9b6cc99ba8869af845b8a6
ecomptonn <162512945+ecomptonn@users.noreply.github.com>
2025-01-08 12:40:17 -0700

    Created modal backdrop blur on page load

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function () {
+    document.getElementById("modalBackdrop").classList.add("show");
     setTimeout(() => {
         document.querySelector(".popup").style.display = "block";
     }, 1000);
